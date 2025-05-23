20caeb096b5238070c20a98ae701a067fded8cc0
CyanideCN <dpy274555447@gmail.com>
2025-05-23 14:49:40 -0700

    chore

--- test_aivis_floater.py
+++ test_aivis_floater.py
@@ -30,7 +30,7 @@ def get_locs(ref_lonlat, step=500, min_x=0, min_y=0, max_x=5500, max_y=5500):
 
     real_shape = ( (max_x - min_x) , (max_y - min_y) )
     numx, numy = int(real_shape[0] / step), int(real_shape[1] / step)
-    
+
     rangex = [(min_x + step * i, min_x + step * i + 500) for i in range(numx)]
     rangey = [(min_y + step * i, min_y + step * i + 500) for i in range(numy)]
     if rangex[-1][-1] < max_x:
@@ -56,12 +56,12 @@ def crop_data(lonlat, datas, basemap, rmin, rmax, cmin, cmax, pad=0):
 
     lon_cropped, lat_cropped, ranges, numy, excess_x, excess_y = get_locs(lonlat, step=500-pad, min_x=rmin, max_x=rmax, min_y=cmin, max_y=cmax)
     basemap_cropped = np.array([basemap[int(cx[0]*mp):int(cx[1]*mp),int(cy[0]*mp):int(cy[1]*mp)] for cx, cy in ranges])
-    
+
     datas_cropped = []
     for data in datas:
         dc = np.array([data[int(cx[0]*mp):int(cx[1]*mp),int(cy[0]*mp):int(cy[1]*mp)] for cx, cy in ranges])
         datas_cropped.append(dc)
-    
+
     return lon_cropped, lat_cropped, datas_cropped, basemap_cropped, numy, excess_x, excess_y
 
 def Feather(data1, data2, pad = 20, axis=0):
@@ -102,16 +102,13 @@ def merge_blocks(aivis_list, numy, excess_x, excess_y, pad, upscale_f):
     return aivis
 
 def run_aivis_floater(files, lonc, latc, lonr, latr, upscale=True, center=True, path_map='./aivis/basemap/himawari8.npz', pad=0, model='1.0'):
-    lat, lon = None, None
-    crop_with_lonlat = False
-    flip_lon = True
-    sat="Himawari8"
+    sat = "Himawari8"
 
-    scn2data = utils.SCENE2DATA(crop_with_lonlat=crop_with_lonlat, flip_lon=flip_lon, lon=lon, lat=lat, sat=sat)
+    scn2data = utils.SCENE2DATA(sat=sat)
     lons, lats, datas, basemap, utc_time, sat_lon, sat_lat, sat_alt = scn2data.get_datas_from_satpy(path_map, files, SAT_READER_HIM, AIVIS_BANDS_HIM)
 
     rmin, rmax, cmin, cmax = scn2data.get_area_bound(latc-latr, latc+latr, lonc-lonr, lonc+lonr)
-    cc, rc = scn2data._proj_inverse(lonc, latc, scn2data.sat)
+    cc, rc = scn2data._proj_inverse(lonc, latc)
     r_off, c_off = 0, 0
     if center:
         r_off = (250 - (int(round(rc)) - rmin) % (500 - pad)) % (500 - pad)
@@ -120,7 +117,7 @@ def run_aivis_floater(files, lonc, latc, lonr, latr, upscale=True, center=True, 
         cmin -= c_off
     lon_cropped, lat_cropped, datas_cropped, basemap_cropped, numy, excess_x, excess_y = crop_data((lons, lats), datas, basemap, rmin, rmax, cmin, cmax, pad)
     bt08_cropped, bt09_cropped, bt10_cropped, bt11_cropped, bt13_cropped, bt15_cropped, bt16_cropped = datas_cropped
-    
+
     print(utc_time.strftime("%Y/%m/%d %H:%M:%S"))
 
     ai_vis = aivis.AI_VIS(gpu_id='0')
@@ -176,7 +173,7 @@ def plot_aivis(data, lon, lat, lonc, latc, lonr, latr):
     )
     plt.clf()
     plt.close('all')
-    
+
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description="AI-VIS test module")
